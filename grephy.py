dcbb75238189d081d10074ec248fab94305c4888
Frank Siderio <frank.siderio1@marist.edu>
2018-04-12 14:09:25 -0400

    adding params to running grephy file

--- grephy.py
+++ grephy.py
@@ -1,18 +1,11 @@
 import os
 import sys
 
-input = raw_input('Enter grephy command: ')
-input = str.split(input)
-
-if(input[0] != "grephy"):
-    print(input[0] + " is not a valid input")
-    # Restarts the script
-    os.execl(sys.executable, sys.executable, *sys.argv)
+file = str(sys.argv[1])
 
 # Check if file exists
-if(os.path.isfile(input[1])):
+if(os.path.isfile(file)):
     print("Found file!")
-    file = open(input[1])
+    file = open(file)
 else:
     print("File not found. Try again")
-    os.execl(sys.executable, sys.executable, *sys.argv)
