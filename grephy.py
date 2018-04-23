37f5cda51e671e0232663153895cb3a9541e446b
Frank Siderio <frank.siderio1@marist.edu>
2018-04-23 18:34:51 -0400

    adding check if file is empty

--- grephy.py
+++ grephy.py
@@ -6,6 +6,8 @@ file = str(sys.argv[1])
 # Check if file exists
 if(os.path.isfile(file)):
     print("Found file!")
+    if(os.stat(file).st_size == 0):
+        print("File is empty")
     file = open(file)
 else:
     print("File not found. Try again")
