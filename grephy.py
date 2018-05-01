2bb8e8fc9c69890d71f904b0ce86d28ce3cabcd7
Frank Siderio <frank.siderio1@marist.edu>
2018-05-01 16:28:34 -0400

    checking if alphabet matches

--- grephy.py
+++ grephy.py
@@ -1,13 +1,28 @@
 import os
 import sys
 
-file = str(sys.argv[1])
+inputFile = str(sys.argv[1])
+regexFile = str(sys.argv[2])
 
 # Check if file exists
-if(os.path.isfile(file)):
-    print("Found file!")
-    if(os.stat(file).st_size == 0):
-        print("File is empty")
-    file = open(file)
+if(os.path.isfile(inputFile) and os.path.isfile(regexFile)):
+    print("Found files!")
+    if(os.stat(inputFile).st_size == 0 and os.stat(regexFile).st_size == 0):
+        print("Files are empty")
+    inputFile = open(inputFile)
+    regexFile = open(regexFile)
+
+    with inputFile as f:
+        input = f.readline()
+    with regexFile as f:
+        regex = f.readline()
+
+    # Get alphabet from regex
+    alphabet = regex.translate(None, '*()')
+
+    if(sorted(alphabet) == sorted(input)):
+        print "Alphabet matches!"
+    else:
+        print "Input is not in the alphabet"
 else:
     print("File not found. Try again")
